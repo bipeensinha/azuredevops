deacd9bc58e14cf96b221a1f2a44c1fdbbd1746b
bipin.sinhaa@outlook.com <bipin.sinhaa@outlook.com>
2021-01-12 10:45:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        This Project Is to Developed for TechPledge Consulting . The only objective of this project to showcase how 
+        react app get deployed in azure app with Azuredevops
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
